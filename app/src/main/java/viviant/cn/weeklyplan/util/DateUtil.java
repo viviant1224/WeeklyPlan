aae6c8a850d6beff0e06847480eacc45c4a84746
weiwei.huang <weiwei.huang@tcl.com>
2016-05-11 13:51:34 +0800

    fix DateUtil

--- app/src/main/java/viviant/cn/weeklyplan/util/DateUtil.java
+++ app/src/main/java/viviant/cn/weeklyplan/util/DateUtil.java
@@ -1,6 +1,9 @@
 package viviant.cn.weeklyplan.util;
 
 
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -39,5 +42,29 @@ public class DateUtil {
 
     public static int getTimeDay(String time) {
         return Integer.parseInt(time.split(" ")[0].split("-")[2]);
+    }
+
+    public static Calendar getCalendar(String time) {
+        try {
+    //      2016-05-10 16:36
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            Date date =sdf.parse(time);
+            Calendar calendar = Calendar.getInstance();
+
+            calendar.setTime(date);
+
+            return calendar;
+        } catch (Exception e) {
+            Log.d("weiwei","time translate error");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static int getCurrentMonth() {
+        Calendar cal = Calendar.getInstance();
+        Date today = new Date();
+        cal.setTime(today);
+        return cal.get(Calendar.MONTH) + 1;
     }
 }
