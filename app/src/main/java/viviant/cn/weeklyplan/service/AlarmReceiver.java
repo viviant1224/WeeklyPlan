a0968d30603382c7c62daeebaba0f01aaffece07
weiwei.huang <weiwei.huang@tcl.com>
2016-06-02 14:21:40 +0800

    update notification util

--- app/src/main/java/viviant/cn/weeklyplan/service/AlarmReceiver.java
+++ app/src/main/java/viviant/cn/weeklyplan/service/AlarmReceiver.java
@@ -50,20 +50,20 @@ public class AlarmReceiver extends BroadcastReceiver {
             e.printStackTrace();
         }
 
-        if (isOpenNotification.equals("true")) {
+        Planthing planthing = (Planthing)intent.getSerializableExtra(Constants.NOTIFICATION_PLAN_THING);
+
+        if (isOpenNotification.equals("true") && planthing.getFlagRemind()) {
             NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
             NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
-
-            Planthing planthing = (Planthing)intent.getSerializableExtra(Constants.NOTIFICATION_PLAN_THING);
 
             mBuilder.setContentTitle(planthing.getPlanthingName())//设置通知栏标题
                     .setContentText(planthing.getPlanthingDescription())
                     .setContentIntent(getDefalutIntent(Notification.FLAG_AUTO_CANCEL, context)) //设置通知栏点击意图
 //  .setNumber(number) //设置通知集合的数量
-                    .setTicker("该做事了") //通知首次出现在通知栏，带上升动画效果的
+                    .setTicker(context.getString(R.string.notification_ticker)) //通知首次出现在通知栏，带上升动画效果的
                     .setWhen(System.currentTimeMillis())//通知产生的时间，会在通知信息里显示，一般是系统获取到的时间
                     .setPriority(Notification.PRIORITY_DEFAULT) //设置该通知优先级
-//  .setAutoCancel(true)//设置这个标志当用户单击面板就可以让通知将自动取消
+                    .setAutoCancel(true)//设置这个标志当用户单击面板就可以让通知将自动取消
                     .setOngoing(false)//ture，设置他为一个正在进行的通知。他们通常是用来表示一个后台任务,用户积极参与(如播放音乐)或以某种方式正在等待,因此占用设备(如一个文件下载,同步操作,主动网络连接)
                     .setDefaults(Notification.DEFAULT_VIBRATE)//向通知添加声音、闪灯和振动效果的最简单、最一致的方式是使用当前的用户默认设置，使用defaults属性，可以组合
                             //Notification.DEFAULT_ALL  Notification.DEFAULT_SOUND 添加声音 // requires VIBRATE permission
@@ -74,7 +74,7 @@ public class AlarmReceiver extends BroadcastReceiver {
 
             mNotificationManager.notify(Integer.parseInt(planthing.getId()+""), mBuilder.build());
 
-            Toast.makeText(context, "闹铃响了, 可以做点事情了~~", Toast.LENGTH_LONG).show();
+            Toast.makeText(context, context.getString(R.string.notification_toast), Toast.LENGTH_LONG).show();
         }
 
 
--- app/src/main/java/viviant/cn/weeklyplan/service/NotificationUtil.java
+++ app/src/main/java/viviant/cn/weeklyplan/service/NotificationUtil.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import viviant.cn.weeklyplan.R;
 import viviant.cn.weeklyplan.bean.Planthing;
 import viviant.cn.weeklyplan.constant.Constants;
 import viviant.cn.weeklyplan.util.DateUtil;
@@ -26,7 +28,18 @@ public class NotificationUtil {
         AlarmManager manager = (AlarmManager) context.getSystemService(context.ALARM_SERVICE);
         manager.set(AlarmManager.RTC_WAKEUP, DateUtil.translateDateTime(planthing.getDoDateTime()), sender);
 
-        Toast.makeText(context, "设置通知成功!", Toast.LENGTH_LONG).show();
+        Toast.makeText(context, context.getString(R.string.set_notification_success), Toast.LENGTH_LONG).show();
 
+    }
+
+    public static void cancelNotification(Context context, Planthing planthing) {
+
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        PendingIntent sender = PendingIntent.getBroadcast(context, Integer.parseInt(planthing.getId()+""), intent, 0);
+        // 取消闹铃
+        AlarmManager am = (AlarmManager)context.getSystemService(context.ALARM_SERVICE);
+        am.cancel(sender);
+
+        Toast.makeText(context, context.getString(R.string.cancel_notification_success), Toast.LENGTH_LONG).show();
     }
 }
